475d5e67cc878ea21fbab7708412dd4579c621ce
Bhushan Pawar <113532749+bhushanpawar94@users.noreply.github.com>
2022-10-21 19:37:04 +0530

    Add files via upload

---
+++ 0. Home Work/StringCase.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class StringCase
+{
+	static void convertString(StringBuffer str)
+{
+	int length = str.length();
+	
+	for (int i =0; i<length;i++)
+	{
+		Character c= str.charAt(i);
+		if (Character.isLowerCase(c))
+		{
+			str.replace(i,i+1,Character.toUpperCase(c)+"");
+		}
+		
+		else
+		{
+			str.replace(i,i+1,Character.toLowerCase(c)+"");
+		}
+	}
+}
+
+public static void main(String agrs[])
+  {
+	Scanner sc = new Scanner(System.in);
+	String s = sc.nextLine();
+	StringBuffer str = new StringBuffer(s);
+	
+	convertString(str);
+	System.out.println(str);
+  }
+}

e50b7911f0b8c89f3b756a52b40d60999a954c48
Bhushan Pawar <113532749+bhushanpawar94@users.noreply.github.com>
2022-11-06 01:37:52 +0530

    Add files via upload

---
+++ 0. Home Work/LAB-1 Assognments/LinkedListIterative.java
@@ -0,0 +1,92 @@
+
+
+
+public class LinkedListIterative   
+{  
+  
+
+static LinkedListNode head;  
+  
+static class LinkedListNode   
+{  
+  
+int val;   
+LinkedListNode next;  
+  
+
+LinkedListNode(int no)  
+{  
+val = no;  
+next = null;  
+}  
+}  
+  
+
+LinkedListNode reverse(LinkedListNode node)  
+{  
+
+LinkedListNode previous = null;  
+LinkedListNode curr = node;  
+LinkedListNode next = null;  
+  
+  
+while (curr != null)   
+{  
+next = curr.next;  
+curr.next = previous;  
+previous = curr;  
+curr = next;  
+}  
+node = previous;  
+return node;  
+}  
+  
+
+void printList(LinkedListNode nde)  
+{  
+while (nde != null)   
+{  
+System.out.print(nde.val + " ");  
+nde = nde.next;  
+}  
+}  
+  
+
+public static void main(String argvs[])  
+{  
+  
+LinkedListIterative listObj = new LinkedListIterative();  
+  
+
+listObj.head = new LinkedListNode(4);  
+  
+  
+listObj.head.next = new LinkedListNode(6);  
+  
+
+listObj.head.next.next = new LinkedListNode(7);  
+  
+
+listObj.head.next.next.next = new LinkedListNode(1);  
+  
+
+listObj.head.next.next.next.next = new LinkedListNode(5);  
+  
+
+listObj.head.next.next.next.next.next = new LinkedListNode(8);  
+  
+
+listObj.head.next.next.next.next.next.next = new LinkedListNode(3);  
+  
+
+listObj.head.next.next.next.next.next.next.next = new LinkedListNode(2);  
+  
+  
+System.out.println("The Linked list before reversal is: ");  
+listObj.printList(head);  
+head = listObj.reverse(head);  
+System.out.println("\n");  
+System.out.println("After reversal, the linked list is: ");  
+listObj.printList(head);  
+}  
+}
